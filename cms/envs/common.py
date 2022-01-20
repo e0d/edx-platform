811e534cf4364f775bd4a6957111dda92fd933a4
Kyle McCormick <kyle@tcril.org>
2022-01-20 18:46:59 -0500

    refactor: move coursegraph to cms
    This code was originally located at:
      ./openedx/core/djangoapps/coursegraph
    
    However, code makes more sense within the ./cms tree, because:
    * it is responsible for publishing course content to an
      external system, with is within the responsibilities of CMS, and
    * is uses modulestore, which is discouraged for use in LMS
      (see 0011-limit-modulestore-use-in-lms.rst).
    
    So, we move the code to:
      ./cms/djangoapps/coursegraph
    and uninstall coursegraph from LMS.
    
    We do not expect this refactor to have any breaking downstream effects.

--- cms/envs/common.py
+++ cms/envs/common.py
@@ -1591,7 +1591,7 @@ INSTALLED_APPS = [
     'openedx.core.djangoapps.self_paced',
 
     # Coursegraph
-    'openedx.core.djangoapps.coursegraph.apps.CoursegraphConfig',
+    'cms.djangoapps.coursegraph.apps.CoursegraphConfig',
 
     # Credit courses
     'openedx.core.djangoapps.credit.apps.CreditConfig',
